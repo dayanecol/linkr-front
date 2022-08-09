6fa4b3e87c4c508fb92dd6afbb8e5d080091378d
emily <nakanodepaula@gmail.com>
2022-08-09 10:44:17 -0300

    feat: add timeline mobile

--- src/components/post.js
+++ src/components/post.js
@@ -6,13 +6,13 @@ export default function Post() {
                 <img src="https://blog.emania.com.br/wp-content/uploads/2019/01/como-tirar-foto-de-cachorro.jpg" alt="imagem teste" />
             </div>
             <div>
-                <h2>Juvenal junior</h2>
-                <h3>Muito maneiro esse tutorial de Material UI com React, deem uma olhada! #react #material</h3>
+                <h2 className="name">Juvenal junior</h2>
+                <h2 className="text">Muito maneiro esse tutorial de Material UI com React, deem uma olhada! #react #material</h2>
                 <div className="box">
                     <div>
-                        <h4>Como aplicar o Material UI em um projeto React</h4>
-                        <h5>Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.</h5>
-                        <h6>https://medium.com/@pshrmn/a-simple-react-router</h6>
+                        <h2 className="namePost">Como aplicar o Material UI em um projeto React</h2>
+                        <h2 className="textPost">Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.</h2>
+                        <h2 className="linkPost">https://medium.com/@pshrmn/a-simple-react-router</h2>
                     </div>
                     <img src="https://blog.emania.com.br/wp-content/uploads/2019/01/como-tirar-foto-de-cachorro.jpg" alt="imagem teste" />
                 </div>
@@ -22,7 +22,7 @@ export default function Post() {
 }
 const Container=styled.div`
     width: 100%;
-    min-height: 276px;
+    min-height: 232px;
 
     background: #171717;
     border-radius: 16px;
@@ -30,75 +30,63 @@ const Container=styled.div`
     display:flex;
 
     margin-bottom:20px;
-    padding:0 10px;
-    
     h2 {
         font-family: 'Lato';
         font-style: normal;
         font-weight: 400;
+    }
+    h2.name {
         font-size: 19px;
         line-height: 23px;
         color: #FFFFFF;
     }
-    h3 {
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 400;
+    h2.text {
         font-size: 17px;
         line-height: 20px;
         color: #B7B7B7;  
     }
-    h4 {
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 400;
+    h2.namePost {
         font-size: 11px;
         line-height: 13px;
-
         color: #CECECE;
     }
-    h5 {
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 400;
+    h2.textPost {
         font-size: 9px;
         line-height: 11px;
-
         color: #9B9595;
     }
-    h6 {
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 400;
+    h2.linkPost {
         font-size: 9px;
         line-height: 11px;
-
         color: #CECECE;
     }
     > * {
         &:nth-child(2){
-            width: 90%;
+            padding:15px;
+            width: 80%;
             gap:5px;
-            padding:20px;
             display:flex;
             flex-direction: column;
+            align-items:start;
         }
     }
     > * {
         &:first-child{
-            width: 10%;
-            margin-right:5px;
-                img {
-                    margin-top:15px;
-                    width: 50px;
-                    height: 50px;
-                    border-radius: 26.5px;
-                }
+            width: 70px;
+            display:flex;
+            align-items:start;
+            justify-content:start;
+            img {
+                margin:15px;
+                width: 50px;
+                height: 50px;
+                border-radius: 26.5px;
+            }
         }
     }
     .box {
         width: 100%;
-        min-height: 115px;
+        height: 115px;
 
         border: 1px solid #4D4D4D;
         border-radius: 11px;
@@ -107,12 +95,17 @@ const Container=styled.div`
             width:70%;
             display:flex;
             flex-direction:column;
-            padding:5px;
+            padding:10px;
             justify-content:space-between;
         }
         img {
             width:30%;
             min-width:95px;
+            border-radius: 0px 12px 13px 0px;
         }
     }
+    @media(max-width: 767px) {
+        border-radius: 0;
+    }
+    
 `
