3525d8b6db6482ea4f45b45ac94b7e8e65ce351a
emily <nakanodepaula@gmail.com>
2022-08-09 00:53:33 -0300

    layout: timeline's place and trending'splace incomplete

---
+++ src/components/createPost.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+export default function CreatePost() {
+    return(
+        <Container>
+            place's create post
+        </Container>
+    )
+}
+const Container = styled.div`
+    width: 100%;
+    height: 209px;
+
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 16px;
+`
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,5 +1,44 @@
 import styled from "styled-components";
 const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background:#333333;;
+`;
+const Main = styled.div`
+    display: flex;
+    flex-direction:row;
+
+    align-items:center;
+    justify-content: space-between;
+
+    margin:0 auto;
+    width: 70%;
+    h1 {
+        font-family: 'Oswald';
+        font-style: normal;
+        font-weight: 700;
+        color: #FFFFFF;
+    }
+    .timeline {
+        width: 70%;
+        display:flex;
+        flex-direction:column;
+        h1 {
+            font-size: 43px;
+            margin: 50px 0;
+        }
+    }
+    .trending {
+        margin-top:150px;
+        width: 28%;
+        height: 40px;
+        left: 877px;
+        top: 232px;
+
+        background: #171717;
+        border-radius: 16px;
+    }
+
 `;
 const Header = styled.div`
     display:flex;
@@ -25,4 +64,4 @@ const Header = styled.div`
         border-radius: 26.5px;
     }
 `
-export {Container, Header}
+export {Main, Header, Container}
